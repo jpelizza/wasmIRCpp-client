c0487b8eb35d3e94b3a45e51f8bb248e998fed23
joaop <joaop@github.com>
2023-04-01 12:57:48 -0300

    adding missing declarations

--- include/ircController.hpp
+++ include/ircController.hpp
@@ -37,38 +37,60 @@ class ircController {
     std::vector<std::string> getChannels();
 
     // actual IRC commands
-    void away(std::string away_msg);
-    void admin(std::string server);
-    void die(std::string server);
+    bool away(std::string away_msg);
+    bool admin(std::string server);
+    bool die(std::string server);
     bool eline(std::vector<std::string> userAThost, std::string duration, std::string reason);
     bool gline(std::vector<std::string> userAThost, std::string duration, std::string reason);
-    void info();
+    bool info();
     bool ison(std::vector<std::string> nick);
     bool join(std::vector<std::string> chans, std::vector<std::string> keys);
     bool invite(std::string nick, std::string chan, std::string duration);
     bool kick(std::string chan, std::vector<std::string> nicks, std::string reason);
     bool kill(std::vector<std::string> nicks, std::string reason);
     bool kline(std::vector<std::string> userAThost, std::string duration, std::string reason);
-    void commands();
-    void privmsg(std::string text);
-    void quit(std::string msg);
+    bool commands();
+    bool privmsg(std::string text);
+    bool quit(std::string msg);
     bool part(std::vector<std::string> chans, std::string reason = "");
     bool list(std::string patterns);
     bool loadModule(std::string module);
-    void lusers();
+    bool lusers();
     bool mode(std::string target, std::string modes, std::vector<std::string> params);
-    void modules();
-    void motd(std::string server);
-    void names(std::vector<std::string> chans);
+    bool modules();
+    bool motd(std::string server);
+    bool names(std::vector<std::string> chans);
     bool nick(std::string nickname);
     bool notice(std::vector<std::string> targets, std::string message);
+    bool oper(std::string name, std::string password);
+    // bool part(std::vector<std::string> chans, std::string reason);
+    // bool pass(std::string password);
+    // bool ping(std::string cookie, std::string server);
+    bool pong(std::string cookie, std::string server);
+    // bool qline(std::vector<std::string> nick, std::string duration, std::string reason);
+    // bool quit(std::string message);
+    // bool rehash(std::string srvType);
+    // bool reloadmodule(std::string module);
+    // bool restart(std::string server);
+    // bool servlist(std::string nick, std::string operType);
+    // bool squery(std::string target, std::string message);
+    // bool stats(std::string character, std::string server);
+    // bool time(std::string server);
+    // bool topic(std::string chan, std::string newTopic);
+    // bool unloadmodule(std::string module);
+    // bool user(std::string username, std::string hostname, std::string servername, std::string realname);
+    // bool userhost(std::vector<std::string> nicks);
+    // bool version(std::string server);
+    // bool wallops(std::string message);
+    // bool who(std::string pattern_b, std::string flags, std::string fields, std::string queryType, std::string pattern_e);
+    // bool whois(std::string server, std::vector<std::string> nicks);
+    // bool whowas(std::string nick, std::string count);
+    // bool zline(std::vector<std::string> ipaddr, std::string duration, std::string reason);
+
     // void commands();
 
     // not exported
     void categorizeMsg(std::string msg);
-
-   private:
-    void pong(std::string server);
 };
 
 #endif  // ircController
